8ce7218b16a58d7c385934026b8595b88dbcc4b5
minji <swij1004@naver.com>
2022-09-20 14:03:51 +0900

    헤더 스타일 변경, 폰트 적용

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,10 @@ module.exports = {
       xl: "1280px",
       // => @media (min-width: 1280px) { ... }
     },
+    fontFamily: {
+      sans: ["Pretendard", "Arial", "sans-serif"],
+      // sans가 제일 기본 상속 폰트이므로 전체 폰트바꾸려면 sans재지정후 맨앞에 원하는 폰트 넣기
+    },
   },
   plugins: [],
 };
